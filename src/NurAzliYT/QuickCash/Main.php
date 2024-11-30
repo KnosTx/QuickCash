40fb94f38a3966b22904544b7c839f3fdc8598e7
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-11-30 17:48:06 +0700

    Update Main.php

--- src/NurAzliYT/QuickCash/Main.php
+++ src/NurAzliYT/QuickCash/Main.php
@@ -7,16 +7,18 @@ use pocketmine\utils\Config;
 use NurAzliYT\QuickCash\api\QuickCashAPI;
 use NurAzliYT\QuickCash\commands\{SetCashCommand, SeeCashCommand, AddCashCommand, RemoveCashCommand, PayCommand};
 
-class Main extends PluginBase {
+class Main extends PluginBase{
 
     private Config $config;
 
-    public function onEnable(): void {
-        // Memuat konfigurasi
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function onEnable() : void{
         $this->saveDefaultConfig();
         $this->config = $this->getConfig();
 
-        // Daftarkan command dengan namespace kosong
         $this->getServer()->getCommandMap()->registerAll(null, [
             new SetCashCommand($this),
             new SeeCashCommand($this),
@@ -26,23 +28,23 @@ class Main extends PluginBase {
         ]);
     }
 
-    public function getConfigValue(string $key, $default = null) {
+    public function getConfigValue(string $key, $default = null){
         return $this->config->get($key, $default);
     }
 
-    public function getCurrencyName(): string {
+    public function getCurrencyName() : string{
         return $this->getConfigValue("currency-name", "Cash");
     }
 
-    public function getDefaultBalance(): int {
+    public function getDefaultBalance() : int{
         return $this->getConfigValue("default-balance", 1000);
     }
 
-    public function getMaxBalance(): int {
+    public function getMaxBalance() : int{
         return $this->getConfigValue("max-balance", 1000000);
     }
 
-    public function getAPI(): QuickCashAPI {
+    public function getAPI() : QuickCashAPI{
         return new QuickCashAPI($this);
     }
 }
