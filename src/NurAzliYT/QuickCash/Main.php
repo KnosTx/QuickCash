9e18767d43c1162fd7a6a09871cb56eba10676ad
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-11-30 17:58:48 +0700

    Update Main.php

--- src/NurAzliYT/QuickCash/Main.php
+++ src/NurAzliYT/QuickCash/Main.php
@@ -3,6 +3,11 @@
 namespace NurAzliYT\QuickCash;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginDescription;
+use pocketmine\plugin\PluginLoader;
+use pocketmine\plugin\ResourceProvider;
+use pocketmine\scheduler\TaskScheduler;
+use pocketmine\Server;
 use pocketmine\utils\Config;
 use NurAzliYT\QuickCash\api\QuickCashAPI;
 use NurAzliYT\QuickCash\commands\{SetCashCommand, SeeCashCommand, AddCashCommand, RemoveCashCommand, PayCommand};
@@ -10,9 +15,31 @@ use NurAzliYT\QuickCash\commands\{SetCashCommand, SeeCashCommand, AddCashCommand
 class Main extends PluginBase{
 
     private Config $config;
+    private PluginLogger $logger;
+	private TaskScheduler $scheduler;
 
-    public function __construct($loader, $server, $description, $dataFolder, $file, $resourceProvider){
-        parent::__construct();
+    public function __construct(
+		private PluginLoader $loader,
+		private Server $server,
+		private PluginDescription $description,
+		private string $dataFolder,
+		private string $file,
+		private ResourceProvider $resourceProvider
+	){
+		$this->dataFolder = rtrim($dataFolder, "/" . DIRECTORY_SEPARATOR) . "/";
+		//TODO: this is accessed externally via reflection, not unused
+		$this->file = rtrim($file, "/" . DIRECTORY_SEPARATOR) . "/";
+		$this->resourceFolder = Path::join($this->file, "resources") . "/";
+
+		$this->configFile = Path::join($this->dataFolder, "config.yml");
+
+		$prefix = $this->description->getPrefix();
+		$this->logger = new PluginLogger($server->getLogger(), $prefix !== "" ? $prefix : $this->getName());
+		$this->scheduler = new TaskScheduler($this->getFullName());
+
+		$this->onLoad();
+
+		$this->registerYamlCommands();
     }
 
     public function onEnable() : void{
